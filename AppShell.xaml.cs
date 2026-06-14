0a4d646021364234705640052dc9f078b53dd1cd
Ubad34 <153395328+Spiderdied@users.noreply.github.com>
2026-06-14 16:54:19 +0300

    Update AppShell.xaml.cs

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -4,11 +4,13 @@ namespace Ubad
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        public AppShell(
+            HomePage      home,
+            ProjectsPage  projects,
+            FavoritesPage favorites,
+            SettingsPage  settings)
         {
             InitializeComponent();
-
-            // Register detail routes
             Routing.RegisterRoute("projectdetail", typeof(ProjectDetailPage));
             Routing.RegisterRoute("browser",       typeof(BrowserPage));
         }
